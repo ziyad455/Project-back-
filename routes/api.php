86744b0e0c9fcd6dc1cbcb5a19f68fe94b054e1c
Ghita Essafi <ghitaessafi550@gmail.com>
2026-05-04 14:49:27 +0100

    feat: Connect ServiceRequest to API and add public categories endpoint

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,11 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ServiceRequestController;
 use App\Http\Controllers\RequestOfferController;
 use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\ServiceCategoryController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/categories', [ServiceCategoryController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
@@ -17,6 +19,7 @@ Route::middleware('auth:sanctum')->group(function () {
     // Service Requests
     Route::get('/requests', [ServiceRequestController::class, 'index']);
     Route::post('/requests', [ServiceRequestController::class, 'store']);
+    // Note: order is important! /requests/my must come before /requests/{serviceRequest}
     Route::get('/requests/my', [ServiceRequestController::class, 'myRequests']);
     Route::get('/requests/{serviceRequest}', [ServiceRequestController::class, 'show']);
     Route::post('/requests/{serviceRequest}/complete', [ServiceRequestController::class, 'complete']);
