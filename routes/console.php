19ca238b509e7a4d06348bd78dcbd412f9148a64
Oussama Oumehdi <oumehdioussama@gmail.com>
2026-05-17 02:15:34 +0100

    chore: add daily scheduled cleanup of expired OTP records in console.php

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    \DB::table('email_verifications')->where('expires_at', '<', now())->delete();
+})->daily();
